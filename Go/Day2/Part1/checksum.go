872b927ea2f1cc5aefee8b76767a608a757de6d6
arendon094 <arendon094@gmail.com>
2017-12-04 22:17:17 -0800

    Completed day2 part1

--- Go/Day2/Part1/checksum.go
+++ Go/Day2/Part1/checksum.go
@@ -5,29 +5,81 @@ import (
 	"fmt"
 	"log"
 	"os"
-	// "strconv"
+	"strconv"
 	"strings"
 )
 
 func main() {
-	inputfile, err := os.Open("./input.txt")
+	grid := getGrid()
+	
+	digitsToSum := getDigitsToSum(grid)
+	
+	sum := checksum(digitsToSum)
+
+  fmt.Println("Value for checksum is", sum)
+}
+
+func getNumbers(stringNums []string) []int {
+	nums := make([]int, len(stringNums))
+	for i := range stringNums {
+		parsedNum, err := strconv.Atoi(stringNums[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums[i] = parsedNum
+	}
+
+	return nums
+}
+
+func getGrid() [][]int {
+  inputfile, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer inputfile.Close()
 
-	scanner := bufio.NewScanner(inputfile)
+	scanner := bufio.NewScanner(inputfile)	
+	grid := [][]int{}
+
 	for scanner.Scan() {
 		input := scanner.Text()
 		inputNums := strings.Split(input, "\t")
-		fmt.Println(inputNums)
+		numbers := getNumbers(inputNums)
+		grid = append(grid, numbers)
 	}
+
+	return grid
 }
 
+func getDigitsToSum(grid [][]int) []int {
+	digitsToSum := []int{}
+	
+	for _, row := range grid {
+		var min, max int
+		for _, rownum := range row {
+			if min == 0 {
+				min = rownum
+			}
+			if rownum > max {
+				max = rownum
+			}
+			if rownum < min {
+				min = rownum
+			}
+		}
+		digitsToSum = append(digitsToSum, max-min)
+	}
 
-// func getNumbers(stringNums []string) []int {
-// 	nums := make([]int, len(stringNums))
-// 	for i := range nums {
-// 		parsedNum, err =: str
-// 	}
-// }
+  return digitsToSum
+}
+
+func checksum(digitsToSum []int) int {
+	var sum int
+
+	for i := range digitsToSum {
+		sum += digitsToSum[i]
+	}
+
+	return sum
+}
